2fc5c962c0181d8c6d7c4130b3d5e090096f27ab
sandeep_baraik <sandeep.baraik@intellithink.in>
2023-09-06 22:03:30 +0530

    bug fixed

--- Services/identityService.js
+++ Services/identityService.js
@@ -5,49 +5,52 @@ const moment = require('moment');
 let identityService = {}
 
 identityService.getIdentity = async function (data) {
-    let queryString = `select * from Contact where email = "${data.email}"`;
-    let response = await db.query(queryString);
-    let createRes;
-    let resObj = { "emails": [], "phoneNumbers": [], "secondaryContactIds": [] }
-    if (response && response.length > 0) {
-        let filteredResponse = response.filter(item => item.email == data.email || item.phoneNumber == data.phoneNumber);
-        filteredResponse.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
-        filteredResponse[0].linkPrecedence = 'primary';
-        filteredResponse[0].linkedId = null;
-        for(let i = 1; i < filteredResponse.length; i++) {
-            filteredResponse[i].linkPrecedence = 'secondary';
-            filteredResponse[i].linkedId = filteredResponse[0].id;
-        }        
-        console.log('filteredResponse: ', filteredResponse);
-        let queries = [];
-        for(let item of filteredResponse) {
-            queries.push(`UPDATE Contact SET linkedId = "${item.linkedId}", linkPrecedence = "${item.linkPrecedence}", updatedAt = now() WHERE id = "${item.id}"`);
-            // await db.query(query);
+    try {
+        let queryString = `select * from Contact where email = "${data.email}"`;
+        let response = await db.query(queryString);
+        let createRes;
+        let resObj = { "emails": [], "phoneNumbers": [], "secondaryContactIds": [] }
+        if (response && response.length > 0) {
+            let filteredResponse = response.filter(item => item.email == data.email || item.phoneNumber == data.phoneNumber);
+            filteredResponse.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
+            filteredResponse[0].linkPrecedence = 'primary';
+            filteredResponse[0].linkedId = null;
+            for (let i = 1; i < filteredResponse.length; i++) {
+                filteredResponse[i].linkPrecedence = 'secondary';
+                filteredResponse[i].linkedId = filteredResponse[0].id;
+            }
+            console.log('filteredResponse: ', filteredResponse);
+            let queries = [];
+            for (let item of filteredResponse) {
+                queries.push(`UPDATE Contact SET linkedId = "${item.linkedId}", linkPrecedence = "${item.linkPrecedence}", updatedAt = now() WHERE id = "${item.id}"`);
+            }
+
+            queries.push(`INSERT INTO Contact (phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt) VALUES ("${data.phoneNumber}", "${data.email}",
+                        "${filteredResponse[0].id}", "secondary", now(), now())`);
+            await Promise.allSettled(queries);
+            // createRes = await db.query(createQuery);
+        } else {
+            let createQuery = `INSERT INTO Contact (phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt) VALUES ("${data.phoneNumber}", "${data.email}", null, "primary",
+                               now(), now())`;
+            createRes = await db.query(createQuery);
         }
-        
-        queries.push(`INSERT INTO Contact (phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt) VALUES ("${data.phoneNumber}", "${data.email}",
-                    "${filteredResponse[0].id}", "secondary", now(), now())`);
-        await Promise.allSettled(queries);
-        // createRes = await db.query(createQuery);
-    } else {
-        console.log("data: ", data);
-        let createQuery = `INSERT INTO Contact (phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt) VALUES ("${data.phoneNumber}", "${data.email}", null "primary",
-                           now(), now())`
-        createRes = await db.query(createQuery);
-    }
-    response = await db.query(queryString);
-    if (response && response.length > 0) {
-        for (let item of response) {
-            resObj.emails.push(item.email);
-            resObj.phoneNumbers.push(item.phoneNumber);
-            if (item.linkPrecedence == 'primary') {
-                resObj['primaryContatctId'] = item.id;
-            } else {
-                resObj.secondaryContactIds.push(item.id);
+        response = await db.query(queryString);
+        if (response && response.length > 0) {
+            for (let item of response) {
+                resObj.emails.push(item.email);
+                resObj.phoneNumbers.push(item.phoneNumber);
+                if (item.linkPrecedence == 'primary') {
+                    resObj['primaryContatctId'] = item.id;
+                } else {
+                    resObj.secondaryContactIds.push(item.id);
+                }
             }
         }
+        return resObj;
+    } catch (error) {
+        console.log(error);
     }
-    return resObj;
+
 }
 
 
